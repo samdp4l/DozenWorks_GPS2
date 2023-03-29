2d1cf8ce24013cb68e2289e433d856c10bb5c8af
kazzyboio <azmyr36@gmail.com>
2023-03-29 14:33:05 +0800

    pause, sound manager

--- Escape Life/Assets/Scripts/AzmyrScripts/Clock.cs
+++ Escape Life/Assets/Scripts/AzmyrScripts/Clock.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
+    //NOT FINAL DO NOT TOUCH
+
     [SerializeField] Transform hourHand;
     [SerializeField] Transform minuteHand;
-    [SerializeField] Transform secondHand;
+    
     const float hoursToDegrees = 0f;
     const float minutesToDegrees = 6f;
-   
+
+    
     void Update()
-    {
+    {  
         TimeSpan time = DateTime.Now.TimeOfDay;
         hourHand.localRotation = Quaternion.Euler(-90f, 0f, hoursToDegrees * (float)time.TotalHours);
         minuteHand.localRotation = Quaternion.Euler(-90f, 0f, minutesToDegrees * (float)time.TotalMinutes);       
     }
+
+   
+    //void Start()
+    //{
+    //    minuteHandInitialRotation = minuteHand.localEulerAngles;
+    //}
+
+    //void Update()
+    //{
+    //    TimeSpan time = DateTime.Now.TimeOfDay;
+    //    float hourRotation = hoursToDegrees * (float)time.TotalHours;
+    //    hourHand.localRotation = Quaternion.Euler(270f, 0f, hourRotation);
+
+    //    float minuteRotation = minutesToDegrees * (float)time.TotalMinutes;
+    //    minuteHand.localRotation = Quaternion.Euler(minuteHandInitialRotation.x, minuteHandInitialRotation.y, minuteRotation);
+    //}
 }
