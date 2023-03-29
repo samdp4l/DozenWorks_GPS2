ff39649e1f673d87e5a2d06d1bc6da70bc05d62e
ozaergabriel <ozaergabriel@gmail.com>
2023-03-29 11:10:46 +0800

    Update Trigger.cs

--- Escape Life/Assets/Scripts/Ozaer/Trigger.cs
+++ Escape Life/Assets/Scripts/Ozaer/Trigger.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
-
+    public GameObject panel;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("You Got it!");
+            panel.SetActive(true);
         }
     }
 }
