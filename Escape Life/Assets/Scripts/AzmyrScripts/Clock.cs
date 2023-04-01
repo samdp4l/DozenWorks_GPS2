1d4348c82241d240c9f57bcb100b717accd9bb1b
Khairil Azmyr <azmyr36@gmail.com>
2023-04-01 18:31:18 +0800

    Adjusted clock, sound effects
    clock still has an issue that it starts at the 15 second mark and moved sound manager as a prefab

--- Escape Life/Assets/Scripts/AzmyrScripts/Clock.cs
+++ Escape Life/Assets/Scripts/AzmyrScripts/Clock.cs
@@ -5,35 +5,50 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    //NOT FINAL DO NOT TOUCH
-
     [SerializeField] Transform hourHand;
     [SerializeField] Transform minuteHand;
-    
-    const float hoursToDegrees = 0f;
-    const float minutesToDegrees = 6f;
+    [SerializeField] Transform secondHand;
 
-    
-    void Update()
-    {  
-        TimeSpan time = DateTime.Now.TimeOfDay;
-        hourHand.localRotation = Quaternion.Euler(-90f, 0f, hoursToDegrees * (float)time.TotalHours);
-        minuteHand.localRotation = Quaternion.Euler(-90f, 0f, minutesToDegrees * (float)time.TotalMinutes);       
+    const float hoursToDegrees = 30f;
+    const float minutesToDegrees = 6f;
+    const float secondsToDegrees = 6f;
+
+    [SerializeField] Canvas gameOverCanvas; 
+    [SerializeField] float timer = 1f;
+    bool canvasDisplayed = false;
+
+    float elapsedSeconds = 0f;
+
+    void Start()
+    {
+        hourHand.localRotation = Quaternion.Euler(-90f, 0f, -90f);
+        minuteHand.localRotation = Quaternion.Euler(-90f, 0f, 0f);
+        secondHand.localRotation = Quaternion.Euler(-90f, 0f, -90f); //the second hand keeps starting at 15 seconds instead of 0
     }
 
-   
-    //void Start()
-    //{
-    //    minuteHandInitialRotation = minuteHand.localEulerAngles;
-    //}
+    void UpdateClock()
+    {
+        float currentSeconds = Time.time;
+       
+        elapsedSeconds += Time.deltaTime;
 
-    //void Update()
-    //{
-    //    TimeSpan time = DateTime.Now.TimeOfDay;
-    //    float hourRotation = hoursToDegrees * (float)time.TotalHours;
-    //    hourHand.localRotation = Quaternion.Euler(270f, 0f, hourRotation);
+        float minutesRotation = minutesToDegrees * (currentSeconds / 60f); 
+        float secondsRotation = secondsToDegrees * (currentSeconds % 60f);
 
-    //    float minuteRotation = minutesToDegrees * (float)time.TotalMinutes;
-    //    minuteHand.localRotation = Quaternion.Euler(minuteHandInitialRotation.x, minuteHandInitialRotation.y, minuteRotation);
-    //}
+        minuteHand.localRotation = Quaternion.Euler(-90f, 0f, minutesRotation); 
+        secondHand.localRotation = Quaternion.Euler(-90f, 0f, secondsRotation);
+
+        if (!canvasDisplayed && elapsedSeconds >= timer)
+        {
+            Time.timeScale = 0f;
+            gameOverCanvas.gameObject.SetActive(true); 
+            canvasDisplayed = true; 
+            elapsedSeconds = 0f; 
+        }
+    }
+
+    void Update()
+    {
+        UpdateClock();      
+    }
 }
